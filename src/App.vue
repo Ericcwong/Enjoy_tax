3d45dfbfe159022a33f3a405717fab7be7bb25a8
Eric Wong <ericwong12@live.com>
2021-09-19 19:07:04 -0600

    boilerplate

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,60 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <!-- <label for="CSV">Upload CSV file</label> -->
+    <form action="">
+      <input type="file" id="CSV" accept=".csv" @change="onFileChange" />
+      <input type="submit" @click.prevent="loadCSV" />
+    </form>
+    <table v-for="transaction in state.loadedData" :key="transaction.cardName">
+      <tr>
+        <th>{{ transaction.cardName }}</th>
+      </tr>
+      <tr></tr>
+    </table>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { reactive } from "vue";
+import Papa from "papaparse";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+  setup() {
+    const state = reactive({
+      loadedData: [],
+    });
+    const onFileChange = (e) => {
+      const file = e.target.files[0];
+      Papa.parse(file, {
+        header: true,
+        complete: function(results) {
+          console.log(results.data);
+          sortRequiredData(results.data);
+        },
+      });
+      const sortRequiredData = (data) => {
+        data.forEach((element) => {
+          const transaction = {
+            cardName: element["Card Brand"],
+            netSale: parseFloat(element["Net Sales"].replace(/\$|,/g, "")),
+            tax: parseFloat(element.Tax.replace(/\$|,/g, "")),
+            total: netSalePlusTax(
+              parseFloat(element["Net Sales"].replace(/\$|,/g, "")),
+              parseFloat(element.Tax.replace(/\$|,/g, ""))
+            ),
+          };
+          state.loadedData.push(transaction);
+          console.log(transaction);
+        });
+      };
+      const netSalePlusTax = (a, b) => {
+        return a + b;
+      };
+    };
+    return { state, onFileChange };
+  },
+};
 </script>
 
 <style>
