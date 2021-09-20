0e07e72a40f405b62b00262abccaa5645fffe95e
Eric Wong <ericwong12@live.com>
2021-09-19 20:58:37 -0600

    Feat: Adding and sorting data working

--- src/App.vue
+++ src/App.vue
@@ -5,17 +5,41 @@
       <input type="file" id="CSV" accept=".csv" @change="onFileChange" />
       <input type="submit" @click.prevent="loadCSV" />
     </form>
-    <table v-for="transaction in state.loadedData" :key="transaction.cardName">
+
+    <table>
       <tr>
-        <th>{{ transaction.cardName }}</th>
+        <th>Date</th>
+        <th>Card Name</th>
+        <th>Net sale</th>
+        <th>Tax</th>
+        <th>Total</th>
       </tr>
-      <tr></tr>
+      <tr v-for="transaction in state.loadedData" :key="transaction.cardName">
+        <td @change="totalCredit">{{ transaction.date }}</td>
+        <td>{{ transaction.cardName }}</td>
+        <td>{{ transaction.netSale }}</td>
+        <td>{{ transaction.tax }}</td>
+        <td>${{ transaction.total }}</td>
+      </tr>
+    </table>
+
+    <table>
+      <tr>
+        <th>Visa Total:</th>
+        <td>${{ state.visa }}</td>
+        <th>AMEX Total:</th>
+        <td>${{ state.amex }}</td>
+        <th>MasterCard Total:</th>
+        <td>${{ state.masterCard }}</td>
+        <th>Discover Total:</th>
+        <td>${{ state.discover }}</td>
+      </tr>
     </table>
   </div>
 </template>
 
 <script>
-import { reactive } from "vue";
+import { reactive, onUpdated } from "vue";
 import Papa from "papaparse";
 export default {
   name: "App",
@@ -23,33 +47,68 @@ export default {
   setup() {
     const state = reactive({
       loadedData: [],
+      visa: 0,
+      amex: 0,
+      masterCard: 0,
+      discover: 0,
     });
     const onFileChange = (e) => {
       const file = e.target.files[0];
       Papa.parse(file, {
         header: true,
         complete: function(results) {
-          console.log(results.data);
+          // console.log(results.data);
           sortRequiredData(results.data);
+          totalCredit();
         },
       });
       const sortRequiredData = (data) => {
         data.forEach((element) => {
           const transaction = {
+            date: element.Date,
             cardName: element["Card Brand"],
-            netSale: parseFloat(element["Net Sales"].replace(/\$|,/g, "")),
-            tax: parseFloat(element.Tax.replace(/\$|,/g, "")),
-            total: netSalePlusTax(
-              parseFloat(element["Net Sales"].replace(/\$|,/g, "")),
-              parseFloat(element.Tax.replace(/\$|,/g, ""))
-            ),
+            netSale: element["Net Sales"],
+            tax: element.Tax,
+            total: netSalePlusTax(element["Net Sales"], element.Tax),
           };
           state.loadedData.push(transaction);
-          console.log(transaction);
+          // console.log(transaction);
         });
       };
       const netSalePlusTax = (a, b) => {
-        return a + b;
+        if (a && b != undefined) {
+          let priceOne = a.replace("$", "");
+          let priceTwo = b.replace("$", "");
+          return (parseFloat(priceOne) + parseFloat(priceTwo)).toFixed(2);
+        } else {
+          return;
+        }
+      };
+
+      const totalCredit = async () => {
+        let cardName = await state.loadedData;
+        cardName.forEach((element) => {
+          switch (element.cardName) {
+            case "Visa":
+              // console.log(element.total);
+              state.visa = state.visa + parseFloat(element.total);
+          }
+          switch (element.cardName) {
+            case "Amex":
+              // console.log(element.total);
+              state.amex = state.amex + parseFloat(element.total);
+          }
+          switch (element.cardName) {
+            case "MasterCard":
+              // console.log(element.total);
+              state.masterCard = state.masterCard + parseFloat(element.total);
+          }
+          switch (element.cardName) {
+            case "Discover":
+              // console.log(element.total);
+              state.discover = state.discover + parseFloat(element.total);
+          }
+        });
       };
     };
     return { state, onFileChange };
@@ -58,12 +117,9 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+table {
+  border: 1px solid black;
+  width: 100%;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
